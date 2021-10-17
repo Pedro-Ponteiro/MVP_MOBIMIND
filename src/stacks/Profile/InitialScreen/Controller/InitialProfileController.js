fb03159ede2d4fa2619caa3731f0dd1f8025371a
Pedro-Ponteiro <pedromcsp@outlook.com>
2021-10-16 23:07:55 -0300

    [feat] profile stack displays user meditations data over time

--- src/stacks/Profile/InitialScreen/Controller/InitialProfileController.js
+++ src/stacks/Profile/InitialScreen/Controller/InitialProfileController.js
@@ -1,19 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const InitialProfileController = ({ navigation }) => {
+  // pega todas as variaveis armazenadas e disponibiliza para o usuario
+  const [keyValuePairArray, setKeyValuePairArray] = useState([]);
+  const [textArray, setTextArray] = useState([]);
+
+  useEffect(() => {
+    const getKeys = async () => {
+      try {
+        const keys = await AsyncStorage.getAllKeys();
+        console.log("Chaves", keys);
+        const keyVal = await AsyncStorage.multiGet(keys);
+        console.log("Keyval", keyVal);
+        setKeyValuePairArray(keyVal);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getKeys();
+  }, []);
+
+  useEffect(() => {
+    const getTextArray = async () => {
+      try {
+        const text = keyValuePairArray.map((array) => {
+          const key = array[0];
+          const value = array[1];
+
+          const arrKey = key.split("_");
+          const textName = arrKey[0].replace("@", "");
+          return arrKey[1] === "times"
+            ? `You have meditated ${textName} ${value} times`
+            : `You have meditated ${textName} for ${value} minutes`;
+        });
+        setTextArray(text);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getTextArray();
+  }, [keyValuePairArray]);
+
   return (
     <View
       style={{
         flex: 1,
-        flexDirection: "column",
         justifyContent: "flex-start",
         alignItems: "flex-start",
         marginTop: 24,
         marginLeft: 8,
       }}
     >
-      <Text>Obrigado por utilizar o App!</Text>
+      {textArray.map((key) => (
+        <Text key={key}>{key}</Text>
+      ))}
     </View>
   );
 };
